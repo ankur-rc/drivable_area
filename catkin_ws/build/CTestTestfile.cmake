c33eea232985859b9b12af0b5a95e4897d8331d9
Ankur Roy Chowdhury <ankurrc.1993@gmail.com>
2019-04-06 14:10:34 -0500

    Removed devel and build from being tracked

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/ankurrc/new_volume/usl/drivable_area_project/catkin_ws/src
-# Build directory: /media/ankurrc/new_volume/usl/drivable_area_project/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("catkin_simple")
-subdirs("drivable_area")
