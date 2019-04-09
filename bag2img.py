aa512549b60542e2ba02e85179c1bc5b532d6dcf
Ankur Roy Chowdhury <ankurrc@tamu.edu>
2019-04-09 12:30:36 -0500

    Added ROS node.

---
+++ bag2img.py
@@ -0,0 +1,6 @@
+'''
+Created Date: Saturday April 6th 2019
+Last Modified: Saturday April 6th 2019 2:44:43 pm
+Author: ankurrc
+'''
+
--- catkin_ws/src/drivable_area/src/RoadSegmentation/utils.py
+++ catkin_ws/src/drivable_area/src/RoadSegmentation/utils.py
@@ -34,8 +34,29 @@ def prepare_output(prediction, cv_image, view_size, prediction_confidence, rgb=(
         channels.append(prediction_)
 
     prediction_img = np.dstack(channels)
+    # calculate center of mass of predicted output
+    com = np.argwhere(prediction == 1)
+    if com.shape[0] > 0:
+        com = np.mean(com, axis=0)
+        com = (int(com[1]), int(com[0]))
+        ego_center = (int(prediction.shape[1]/2), prediction.shape[0])
+        # compute the distance and angle (in degrees)
+        # translate to euclidean x-y frame with 'ego center' as the origin
+        com_coords, ego_center_coords = np.array(
+            [com[0], -com[1]]), np.array([ego_center[0], -ego_center[1]])
+        d = com_coords - ego_center_coords
+        r = np.sum(d**2)**0.5
+        theta = np.arctan2(d[1], d[0])*180/np.pi
+
+        # draw them on prediction map
+        cv2.circle(prediction_img, com, 5, (255, 0, 0))
+        cv2.circle(prediction_img, ego_center, 5, (0, 0, 255))
+        cv2.arrowedLine(prediction_img, ego_center, com, (255, 0, 0))
+        ret = cv2.putText(prediction_img, 'r:{:.02f}, theta:{:.02f}'.format(
+            r, theta), (5, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255))
+
     cv_image = (cv_image*255).astype(np.uint8)
-    alpha = 0.2
+    alpha = 0.4
 
     overlay_img = cv2.addWeighted(
         prediction_img, alpha, cv_image, 1 - alpha, 0)
